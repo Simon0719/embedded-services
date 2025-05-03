d9b3a49a3db760032b7e09bf245807335717fe69
Simon0719 <best.betgod@gmail.com>
2025-05-03 22:56:30 +0800

    try

--- type-c-service/src/wrapper/mod.rs
+++ type-c-service/src/wrapper/mod.rs
@@ -45,7 +45,7 @@ pub async fn set_debug_card_port(select_port: u8) {
     assign_port.initial = true;
 }
 
-pub fn set_debug_card_status(status_update: u8, whichPort: u8) {
+pub async fn set_debug_card_status(status_update: u8, whichPort: u8) {
     let dbg_temp = DBG_CARD_STS.lock().await;
     let mut assign_status = dbg_temp.borrow_mut();
     if assign_status.initial == true {
@@ -59,7 +59,7 @@ pub fn set_debug_card_status(status_update: u8, whichPort: u8) {
     }
 }
 
-pub fn get_debug_card_status() -> u8 {
+pub async fn get_debug_card_status() -> u8 {
     let dbg_temp = DBG_CARD_STS.lock().await;
     let report_status = dbg_temp.borrow_mut();
     let sts = report_status.debug_card_connect;
